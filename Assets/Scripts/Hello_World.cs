0af1ff83adbbb40fffe6a0e8e4127c91a16e81ec
odase9-cloud <odase9@gmail.com>
2026-09-15 11:08:18 -0700

    Changed Hello_World script

--- Assets/Scripts/Hello_World.cs
+++ Assets/Scripts/Hello_World.cs
@@ -5,12 +5,12 @@ public class Hello_World : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("This is an new addtion and update");
     }
 }
