1a31e6ff977bf5e630d4ff15649ef44ca40b5e84
odase9-cloud <odase9@gmail.com>
2026-09-22 11:18:15 -0700

    Update Sprite_Changer.cs

--- Assets/Scripts/Sprite_Changer.cs
+++ Assets/Scripts/Sprite_Changer.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Sprite_Changer : MonoBehaviour
 {
-      public SpriteRenderer theRenderer; 
+    public SpriteRenderer theRenderer; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     
